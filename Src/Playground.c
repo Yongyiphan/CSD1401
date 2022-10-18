331c99fc2136093eb5563648bd68aaade993a57e
Edgar <edgar.yong.yip.han@gmail.com>
2022-10-18 15:06:53 +0800

    Tryn to make mob movement

--- Src/Playground.c
+++ Src/Playground.c
@@ -9,7 +9,24 @@ int Width = 500;
 int MobPoolQuantity = 100;
 Mob *MobPoolPtr;
 int waveTrack[5], cWave = 0;
+/*
+Mob C Flow
+: Generate N amt of mobs at random locations
+: Allow Random Movement of Mobs Towards Player
 
+Mob Cycle:
+: Init -> Generate mobs as per waves count;
+: Every "Min" / All Mob on screens' death -> Generate New waves
+------------
+: 5 Wave Pool of memory of size Mob * MobPoolQuantity 
+: Spawn  Wave tracks amt of mobs (Waves Mob <= MobPoolQuantity) else realloc more memory
+: Decrement wave track when mobs die
+	: IF all mobs in waves die. Spawn new waves. 
+
+Print Conditions
+: Waves with mob count > 0
+
+*/
 void game_init(void) {
 
 	CP_System_SetWindowSize(Width, Height);
