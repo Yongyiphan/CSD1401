00e8ac9b1b0427d503fa975289c788f4dd6a89ae
Geoffrey Cho <geoffrey_white@live.com>
2022-10-19 16:40:27 +0800

    Created Functioning healthbar for player

--- Src/map.c
+++ Src/map.c
@@ -10,6 +10,7 @@
 #define PLAYER_DAMAGE 1.0f
 #define ATK_SPEED 2.0f
 #define DEFENSE 10
+#define PLAYER_HITBOX 50
 
 
 Player P;
@@ -19,6 +20,7 @@ CP_Color grey, black, red, blue, white;
 
 
 void map_Init(void) {
+	
 	CP_System_SetWindowSize(MAP_SIZE, MAP_SIZE);
 
 	grey = CP_Color_Create(111, 111, 111, 255);
@@ -26,16 +28,26 @@ void map_Init(void) {
 
 	start_vector = CP_Vector_Zero();
 	// Initialize the coordinates and stats of the player
-	P = (Player){ start_vector.x, start_vector.y, PLAYER_HP, PLAYER_SPEED, PLAYER_DAMAGE, ATK_SPEED, DEFENSE };
+	P = (Player){ start_vector.x, start_vector.y, 90, PLAYER_HP, PLAYER_SPEED, PLAYER_DAMAGE, ATK_SPEED, DEFENSE, PLAYER_HITBOX};
+	
+	CP_Graphics_DrawRect(100, 100, 300, 30);
 	CameraDemo_Init();
 }
 
 void map_Update(void) {
-	
+
+	show_healthbar(P);
 	CameraDemo_Update(P);
+	
 	if (CP_Input_KeyDown(KEY_H)) {
 		P.SPEED *= 1.1;
 	}
+
+
+	if (CP_Input_MouseClicked()) {
+		
+	}
+
 }
 
 void map_Exit(void) {
--- Src/player.c
+++ Src/player.c
@@ -1,17 +1,47 @@
 #include "cprocessing.h"
 #include <stdio.h>
+#include "player.h"
 
-
-#define PLAYER_HITBOX 50.0f
+// Player hitbox is a circle
+#define PLAYER_DIAMETER 50.0f
 #define PLAYER_SPEED 20
 
-int health = 100;
 
 
+int collide_mob(Player p, Mob mob) {
+	if (CP_Math_Distance(p.x, p.y, mob.x, mob.y) < p.HITBOX / 2 + mob.hitbox / 2) {
+		p.CURRENT_HP -= 5;
+		return 1;
+	}
+	return 0;
+}
 
+int collide_item(Player p, Items item) {
+	if (CP_Math_Distance(p.x, p.y, item.x, item.y) < p.HITBOX / 2 + item.hitbox / 2) {
+		// change the player stats or affect the bullet type
+		return 1;
+	}
+	return 0;
+}
 
-//int if_collide(float x, float y, Object item) {
-//	
-//}
+int collide_bullet(Player p, Bullet bullet) {
+	if (CP_Math_Distance(p.x, p.y, bullet.x, bullet.y) < p.HITBOX / 2 + bullet.hitbox / 2) {
+		p.CURRENT_HP -= 5;
+		return 1;
+	}
+	return 0;
+}
 
+void shoot_bullet(Bullet bullet) {
+	
+}
 
+void show_healthbar(Player p) {
+	float x_coord = 40;
+	float y_coord = 20;
+	int rectWidth = 300;
+	int rectHeight = 30;
+	CP_Graphics_DrawRect(x_coord, y_coord, rectWidth, rectHeight);
+	CP_Settings_Fill(CP_Color_Create(255, 0, 0, 255));
+	CP_Graphics_DrawRect(x_coord, y_coord, p.CURRENT_HP / p.MAX_HP * rectWidth, rectHeight);
+}
